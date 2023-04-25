2eb9d01dd8728d4cb7ad78b57b3f601a00952a70
Becca <112990922+rebecacarvalhoaraujo@users.noreply.github.com>
2023-04-25 13:55:36 -0300

    novas atualizações

--- routes/produtos.js
+++ routes/produtos.js
@@ -4,7 +4,8 @@ const Produto = require('../model/Produto');
 const { errorMonitor } = require('events');
 
 produtos.route('/')
-    .get(async (req, res) => {const { nome, preco, categoria } = req.query;
+    .get(async (req, res) => {
+        const { nome, preco, categoria } = req.query;
 
         try {
             if (!nome && !preco && !categoria) {
@@ -30,25 +31,47 @@ produtos.route('/')
 
     })
     .post(async (req, res) => {
-        const { nome, descricao, quantidade, preco, desconto, dataDesconto, categoria, imgProduto } = req.body;
+
+        const putSchema = Joi.object({
+            id: Joi.string().required(),
+            nome: Joi.string().required(),
+            descricao: Joi.string().required(),
+            quantidade: Joi.number().integer().required(),
+            preco: Joi.number().positive().required(),
+            desconto: Joi.number().min(0).max(1).optional(),
+            dataDesconto: Joi.date().iso().optional(),
+            categoria: Joi.string().required(),
+            imgProduto: Joi.string().required()
+        });
+
+        const { error } = putSchema.validate(req.body);
+
+        if (error) {
+            return res.status(400).json({ message: error.details[0].message });
+        }
+
+        const { id, nome, descricao, quantidade, preco, desconto, dataDesconto, categoria, imgProduto } = req.body;
+
         try {
-            if (!nome || !descricao || !quantidade || !preco || !categoria || !imgProduto) {
-                return res.status(400).json({ message: "Campos obrigatórios ausentes." });
+
+            const produtoEncontrado = await Produto.findById(id);
+            if (!produtoEncontrado) {
+                return res.status(404).json({ message: "produto não encontrado!" });
             }
-            if (desconto && dataDesconto) {
-                const produto = new Produto({ nome, descricao, quantidade, preco, desconto, dataDesconto, categoria, imgProduto });
-                await produto.save();
-                res.status(201).json(produto);
+
+            if (typeof desconto !== 'undefined' && typeof dataDesconto !== 'undefined') {
+                const precoComDesconto = preco - (preco * desconto);
+                const response = await Produto.findByIdAndUpdate(id, { nome, descricao, quantidade, preco, desconto, dataDesconto, precoComDesconto, categoria, imgProduto }, { new: true })
+                res.status(200).json(response);
             } else {
-                const produto = new Produto({ nome, descricao, quantidade, preco, categoria, imgProduto });
-                await produto.save();
-                res.status(201).json(produto);
+                const response = await Produto.findByIdAndUpdate(id, { nome, descricao, quantidade, preco, categoria, imgProduto }, { new: true })
+                res.status(200).json(response);
             }
+
         } catch (err) {
             console.log(err);
             res.status(500).json(err);
         }
-
     })
     .put(async (req, res) => {
         const { id, nome, descricao, quantidade, preco, desconto, dataDesconto, categoria, imgProduto } = req.body;
@@ -63,7 +86,7 @@ produtos.route('/')
             }
 
             const response = await Produto.findByIdAndUpdate
-            (id, { nome, descricao, quantidade, preco, desconto, dataDesconto, categoria, imgProduto }, { new: true })
+                (id, { nome, descricao, quantidade, preco, desconto, dataDesconto, categoria, imgProduto }, { new: true })
             if (response) {
                 res.status(200).json(response);
             } else {
@@ -77,6 +100,16 @@ produtos.route('/')
 
     })
     .delete(async (req, res) => {
+        const deleteSchema = Joi.object({
+            id: Joi.string().required()
+        });
+
+        const { error } = deleteSchema.validate(req.body);
+
+        if (error) {
+            return res.status(400).json({ message: error.details[0].message });
+        }
+
         const { id } = req.body;
         try {
             const response = await Produto.findByIdAndRemove(id);
